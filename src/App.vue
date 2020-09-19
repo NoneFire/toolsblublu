652df094abedfbe5eff941f86e5ef3118b789b85
blf <fzz_mx@qq.com>
2020-09-19 23:32:57 +0800

    add headers

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,83 @@
 <template>
-  <div id="app">
+  <!-- <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <h1> 哈喽</h1>
+  </div> -->
+  <div class="container">
+	  
+	  <!-- 引入TheHeader组件 -->
+	  <div id="wrap">
+		  <TheHeader></TheHeader>
+	  </div>
+	  
+	  <br>
+	  <br>
+	  <br>
+	  
+	  <div class="jumbotron">
+		  <div class="container">
+			  <h1>哈喽哈喽</h1>
+			  <p>
+				  这是测试测试
+			  </p>
+			<p>
+				<a class="btn btn-primary btn-lg" href="http://www.bootcss.com/" role="button" target="_blank">
+					<!-- Learn more -->
+					<span class="glyphicon glyphicon-hand-right" aria-hidden="true">
+					</span>
+				</a>
+			</p>
+		  </div>
+		  
+	  </div>
+	  
+
+  
+  <div class="jumbotron">
+	  <div class="container">
+		  <h1>fontawesome</h1>
+		  <p><i>aa这是斜体</i>这是解释说明</p>
+		  <p>
+			  <i class="fa fa-camera"></i>
+			  <i class="fa fa-camera fa-2x"></i>
+			  <i class="fa fa-camera fa-3x"></i>
+			  <i class="fa fa-camera fa-4x"></i>
+			  <i class="fa fa-camera fa-5x"></i>
+		  </p>
+	  </div>
+  </div>
+  
   </div>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from '@/components/layouts/TheHeader'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+	  TheHeader
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-h1 {
-	color: blue;
-}
+<style lang="scss">
+	$container-large-desktop: 1200px;
+	$btn-primary-bg: #00b5ad;
+	$btn-primary-border: #00b5ad;
+	$label-primary-bg: #00b5ad;
+	$pagination-active-bg: #00b5ad;
+	$pagination-active-border: #00b5ad;
+	$pagination-color: #00b5ad;
+	$input-border-focus: #00b5ad;
+	$link-color: #12c4c5;
+	$link-hover-color: #22ddde;
+	$icon-font-path: "~bootstrap-sass/assets/fonts/bootstrap/";
+	$fa-font-path: "~font-awesome/fonts/";
+
+	@import "~bootstrap-sass/assets/stylesheets/_bootstrap";
+	@import "~font-awesome/scss/font-awesome";
+	@import "./styles/main";
+	@import "./styles/extra";
 </style>
---
+++ src/components/layouts/TheHeader.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="navbar navbar-default topnav">
+	  <div class="container">
+		  <div class="navbar-header">
+			  <a href="./" class="navbar-brand">
+				  <img v-bind:src="logo.src" v-bind:alt="logo.title"  height="20px"/>
+			  </a>
+		  </div>
+	  <!-- 导航栏 -->
+		  <div id="top-navbar-collapse" class="collapse navbar-collapse">
+			  <ul class="nav navbar-nav">
+				  <li v-for="item in navList">
+					  <a href="#">{{ item }}</a>
+				  </li>
+			  </ul>
+		  </div>
+	  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TheHeader',
+  data(){
+	  return {
+		  logo:{
+			  src: 'logo.png',
+			  title:"工具集"
+		  },
+		  navList:["数据文件切割","机器学习应用","在线建议留言"]
+	  }
+	  },
+  beforeCreate(){
+	  console.log('这是创建前');
+  }
+}
+</script>
+
+
+<style scoped>
+.title{
+	display: none;
+}
+</style>
